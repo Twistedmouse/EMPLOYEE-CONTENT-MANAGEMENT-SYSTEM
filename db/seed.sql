f4d919aff3abebf4359d39647f5416d269f86491
tristan <mousy93@hotmail.com>
2021-06-30 18:31:02 +0800

    added start of database data base

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE employee_contentDB;
+USE employee_contentDB;
+CREATE TABLE department();
+CREATE TABLE roles();
+CREATE TABLE employees();
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 const inquirer = require("inquirer");
+
+// const connection = mysql2.createConnection({
+//   host: "localhost",
+
+//   port: 3306,
+
+//   user: "root",
+
+//   password: "",
+//   database: "",
+// });
