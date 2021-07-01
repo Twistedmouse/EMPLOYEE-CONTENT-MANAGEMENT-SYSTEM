0393caed471969bc7a73cc1b543c44062c02eb63
tristan <mousy93@hotmail.com>
2021-07-01 15:58:27 +0800

    starting quesetion added

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,22 @@
 CREATE DATABASE employee_contentDB;
 USE employee_contentDB;
-CREATE TABLE department();
-CREATE TABLE roles();
-CREATE TABLE employees();
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id),
+);
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(6, 2),
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+);
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id),
+);
--- server.js
+++ server.js
@@ -1,14 +1,52 @@
 require("dotenv").config();
-const mysql = require("mysql2/promise");
+const mysql2 = require("mysql2/promise");
 const inquirer = require("inquirer");
 
-// const connection = mysql2.createConnection({
-//   host: "localhost",
+//connection info for mysql
+const connection = mysql2.createConnection({
+  host: "localhost",
 
-//   port: 3306,
+  port: 3306,
 
-//   user: "root",
+  user: "root",
 
-//   password: "",
-//   database: "",
-// });
+  password: "password",
+  database: "employee_contentDB",
+});
+
+// console.log(`
+//  _____  _____ ____  _    _    _____ _____ _      _____ ____  ____  _____  ____  ____
+// /__ __\/  __//  _ \/ \__/ |  /  __//  __// \  /|/  __//  __\/  _ \/__ __\/  _ \/  __\
+//   / \  |  \  | / \|| |\/| |  | |  _|  \  | |\ |||  \  |  \/|| / \|  / \  | / \||  \/|
+//   | |  |  /_ | | ||| |  | |  | |_//|  /_ | | \|||  /_ |    /| |-||  | |  | \_/||    /  /\
+//   \_/  \____\\_/ \|\_/  \_|  \____\\____\\_/  \|\____\\_/\_\\_/ \|  \_/  \____/\_/\_\  \/ `);
+
+//array of questions to build out the tables and data for database
+async function questions() {
+  const programStartChoice = await inquirer.prompt([
+    {
+      type: "rawlist",
+      name: "beginChoice",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View ALL departments",
+        "View ALL roles",
+        "View ALL employees",
+        "Update employee role",
+        "Exit program",
+      ],
+    },
+  ]);
+}
+
+//function that is first invoked
+function init() {
+  console.log("intro ascii goes here\n");
+  setTimeout(function () {
+    questions();
+  }, 3000);
+}
+init();
